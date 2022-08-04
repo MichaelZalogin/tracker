056258524e27ba01056022b93147c76042914d47
MichaelZalogin <zaloginmiha@gmail.com>
2022-08-05 01:52:32 +0300

    Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.*;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item1 = new Item();
+        LocalDateTime nowDateTime = item1.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println(nowDateTime.format(formatter));
+    }
+}
