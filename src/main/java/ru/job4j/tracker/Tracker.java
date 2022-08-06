26561d4c2edad9eb1b4739c066f34d7fdebf4414
MichaelZalogin <zaloginmiha@gmail.com>
2022-08-07 02:20:48 +0300

    Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -26,15 +26,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findByName(String key) {
@@ -47,5 +40,27 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(copyItemArray, index);
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item anotherTack) {
+        int index = indexOf(id);
+        if (index != -1) {
+            anotherTack.setId(id);
+            anotherTack.setName(anotherTack.getName());
+            items[index] = anotherTack;
+            return true;
+        }
+        return false;
     }
 }
