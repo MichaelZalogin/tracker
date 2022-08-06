514268d43bae371fc7657cf107ae209877025bf9
MichaelZalogin <zaloginmiha@gmail.com>
2022-08-07 00:55:52 +0300

    Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,18 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] copyItemArray = new Item[items.length];
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                copyItemArray[index] = items[i];
+                index++;
+            }
+        }
+        return Arrays.copyOf(copyItemArray, index);
     }
 
     public Item findById(int id) {
@@ -21,5 +35,17 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] copyItemArray = new Item[items.length];
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            if (key.equals(items[i].getName())) {
+                copyItemArray[index] = items[i];
+                index++;
+            }
+        }
+        return Arrays.copyOf(copyItemArray, index);
     }
 }
