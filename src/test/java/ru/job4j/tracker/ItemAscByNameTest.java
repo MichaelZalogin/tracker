7814f42b0043e1df4b1d26431eff354f7ce8afca
MichaelZalogin <zaloginmiha@gmail.com>
2022-09-10 23:14:37 +0300

    Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ItemAscByNameTest {
+
+    @Test
+    void compare() {
+        Item item1 = new Item(1, "item1");
+        Item item2 = new Item(2, "item2");
+        Item item3 = new Item(3, "item3");
+        Item item4 = new Item(4, "item4");
+        List<Item> items = new ArrayList<>();
+        items.add(item1);
+        items.add(item3);
+        items.add(item4);
+        items.add(item2);
+        List<Item> expected = List.of(item1, item2, item3, item4);
+        items.sort(new ItemAscByName());
+        assertThat(expected).isEqualTo(items);
+    }
+}
