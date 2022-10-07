e68e443d053646b1cad38695f4a6c9e6bba073c0
MichaelZalogin <zaloginmiha@gmail.com>
2022-10-07 22:36:48 +0300

    Singleton [#94606 #334346]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.List;
+
 public class SingleTracker {
 
     private static SingleTracker instance = null;
@@ -19,8 +21,20 @@ public class SingleTracker {
         return tracker.add(item);
     }
 
+    public List<Item> findAll(Item item) {
+        return tracker.findAll();
+    }
+
     public Item findById(int id) {
         return tracker.findById(id);
+    }
+
+    public List<Item> findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item back) {
+        return tracker.replace(id, back);
     }
 
     public boolean delete(int id) {
